965620eb62dc01f9e18ebd77a606d44058016c86
Juan Diego Mejia Maestre <mejiamaestrejuandiego@gmail.com>
2023-06-18 09:41:11 -0500

    eliminar todo del pasajero

--- Session3Simulacro2023/View/ConfirmReserva.cs
+++ Session3Simulacro2023/View/ConfirmReserva.cs
@@ -126,6 +126,8 @@ namespace Session3Simulacro2023 {
 
         private void button2_Click(object sender, EventArgs e) {
             if (MessageBox.Show("¿Desea eliminar el pasajero?", "Alerta", MessageBoxButtons.YesNo, MessageBoxIcon.Question)== DialogResult.Yes) {
+                var pasajero = Pasajeros[DPasajero.CurrentRow.Index];
+                File.Delete(pasajero.URl);
                 Pasajeros.RemoveAt(DPasajero.CurrentRow.Index);
                 DPasajero.DataSource = null;
                 DPasajero.DataSource = Pasajeros;
